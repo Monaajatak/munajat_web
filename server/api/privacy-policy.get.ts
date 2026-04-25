e1c360d9407c0fad27e16f96773bbe9c0d6f6430
Mohamed Elsayed <mohammed.sayed201638@gmail.com>
2026-04-25 23:48:35 +0300

    fix: correct indentation in privacy policy API handler

--- server/api/privacy-policy.get.ts
+++ server/api/privacy-policy.get.ts
@@ -1,6 +1,6 @@
 import policyDocument from '../../privacy_policy.json'
 
 export default defineEventHandler((event) => {
-  setHeader(event, 'content-type', 'application/json; charset=utf-8')
-  return policyDocument
+    setHeader(event, 'content-type', 'application/json; charset=utf-8')
+    return policyDocument
 })
